1023ec53cd963c8489fbb3949251191e3191f147
Wekoslav Stefanovski <swekster@gmail.com>
2017-06-27 19:27:55 +0200

    First Data Driven Application

---
+++ FirstApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "select count(*) from Authors";
+
+            int authorCount = (int)cmd.ExecuteScalar();
+
+            Console.WriteLine(authorCount);
+
+            connection.Close();
+        }
+    }
+}
