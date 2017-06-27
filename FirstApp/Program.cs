740f71eb9e3e514a3f4c1f14e0181a211c773032
Wekoslav Stefanovski <swekster@gmail.com>
2017-06-27 21:35:47 +0200

    added injection and filterong

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -13,7 +13,7 @@ namespace FirstApp
         {
             while (true)
             {
-                FirstFiltering();
+                ThirdFiltering();
             }
         }
 
@@ -64,6 +64,7 @@ namespace FirstApp
             connection.Close();
         }
 
+        // vulnerable - DO NOT USE THIS FORM
         private static void FirstFiltering()
         {
             SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
@@ -74,7 +75,79 @@ namespace FirstApp
 
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = connection;
-            cmd.CommandText = $"select ID, Name, DateOfBirth from Authors where Name like '%{query}%'";
+            // vulnerable - DO NOT USE THIS FORM
+            cmd.CommandText = "select ID, Name, DateOfBirth from Authors where Name like '%" + query + "%'";
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                //int authorId = dr.GetInt32(0);
+                //string name = dr.GetString(1);
+
+                //int authorId = dr.GetFieldValue<int>(0);
+                //string name = dr.GetFieldValue<string>(1);
+
+                int authorId = (int)dr["ID"];
+                string name = (string)dr["Name"];
+                DateTime? dob = dr.IsDBNull(2)
+                    ? (DateTime?)null
+                    : (DateTime)dr["DateOfBirth"];
+
+                Console.WriteLine($"{authorId} - {name} - {dob}");
+            }
+
+            connection.Close();
+        }
+
+        private static void SecondFiltering()
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
+            connection.Open();
+
+            Console.Write("Enter author name: ");
+            string query = Console.ReadLine();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = "select ID, Name, DateOfBirth from Authors where Name like '%'+@authorName+'%'";
+            cmd.Parameters.AddWithValue("@authorName", query);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                //int authorId = dr.GetInt32(0);
+                //string name = dr.GetString(1);
+
+                //int authorId = dr.GetFieldValue<int>(0);
+                //string name = dr.GetFieldValue<string>(1);
+
+                int authorId = (int)dr["ID"];
+                string name = (string)dr["Name"];
+                DateTime? dob = dr.IsDBNull(2)
+                    ? (DateTime?)null
+                    : (DateTime)dr["DateOfBirth"];
+
+                Console.WriteLine($"{authorId} - {name} - {dob}");
+            }
+
+            connection.Close();
+        }
+
+        private static void ThirdFiltering()
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
+            connection.Open();
+
+            Console.Write("Enter author name: ");
+            string query = Console.ReadLine();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "getAuthors";
+            cmd.Parameters.AddWithValue("@authorName", query);
 
             SqlDataReader dr = cmd.ExecuteReader();
 
