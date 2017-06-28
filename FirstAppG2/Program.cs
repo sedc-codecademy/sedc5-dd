d3d24c230bfe54ffee3b369c3a9f026fe8a72737
Wekoslav Stefanovski <swekster@gmail.com>
2017-06-28 19:14:04 +0200

    First app for g2

---
+++ FirstAppG2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "select * from Authors";
+
+            var result = command.ExecuteScalar();
+
+            Console.WriteLine(result);
+
+            connection.Close();
+        }
+    }
+}
