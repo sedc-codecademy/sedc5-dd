dd773b5c04e7f5c615fe07fee1f0ba49c538fc6f
Wekoslav Stefanovski <swekster@gmail.com>
2017-06-28 19:41:24 +0200

    added author list

--- FirstAppG2/Program.cs
+++ FirstAppG2/Program.cs
@@ -11,6 +11,27 @@ namespace FirstAppG2
     {
         static void Main(string[] args)
         {
+            FirstDataRead();
+        }
+
+        private static void FirstDataAccess()
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
+            connection.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "select count(*) from Authors";
+
+            var result = command.ExecuteScalar();
+
+            Console.WriteLine(result);
+
+            connection.Close();
+        }
+
+        private static void FirstDataRead()
+        {
             SqlConnection connection = new SqlConnection("Server=.;Database=BooksDb;Trusted_Connection=True;");
             connection.Open();
 
@@ -18,9 +39,21 @@ namespace FirstAppG2
             command.Connection = connection;
             command.CommandText = "select * from Authors";
 
-            var result = command.ExecuteScalar();
+            var dr = command.ExecuteReader();
 
-            Console.WriteLine(result);
+            while (dr.Read())
+            {
+                //int authorID = (int) dr["ID"];
+                //string authorName = (string) dr["Name"];
+
+                //int authorID = dr.GetInt32(0);
+                //string authorName = dr.GetString(1);
+
+                int authorID = dr.GetFieldValue<int>(0);
+                string authorName = dr.GetFieldValue<string>(1);
+
+                //Console.WriteLine($"{authorID}: {authorName}");
+            }
 
             connection.Close();
         }
