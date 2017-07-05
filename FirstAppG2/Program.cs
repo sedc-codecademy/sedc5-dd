4b59dbd3a4be651c65e638e783209acd90aed5b4
Wekoslav Stefanovski <swekster@gmail.com>
2017-07-05 21:22:42 +0200

    added authorloader

--- FirstAppG2/Program.cs
+++ FirstAppG2/Program.cs
@@ -15,9 +15,18 @@ namespace FirstAppG2
         static void Main(string[] args)
         {
             connectionString = ConfigurationManager.ConnectionStrings["BooksDb"].ConnectionString;
+            var loader = new AuthorLoader(connectionString);
+            
+            //var results = loader.GetAllAuthors();
+            //PrintAuthors(results);
 
-            var results = GetAllAuthors();
-            PrintAuthors(results);
+            while (true)
+            {
+                Console.Write("Enter name fragment: ");
+                var fragment = Console.ReadLine();
+                var results = loader.GetAuthorsByName(fragment);
+                PrintAuthors(results);
+            }
         }
 
         private static void PrintAuthors(IEnumerable<Author> authors)
@@ -236,84 +245,6 @@ namespace FirstAppG2
             }
 
             connection.Close();
-        }
-
-        private static List<Author> GetAuthorsByName()
-        {
-            Console.Write("Enter name fragment: ");
-            string query = Console.ReadLine();
-
-            List<Author> result = new List<Author>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.StoredProcedure;
-                    command.CommandText = "getAuthors";
-                    command.Parameters.AddWithValue("@authorName", query);
-
-                    using (var dr = command.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            result.Add(LoadAuthorForReader(dr));
-                        }
-                    }
-
-                }
-
-            }
-            return result;
-        }
-
-        private static List<Author> GetAllAuthors()
-        {
-            List<Author> result = new List<Author>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                using (SqlCommand command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandText = "select ID, Name, DateOfBirth, DateOfDeath from Authors";
-
-                    using (var dr = command.ExecuteReader())
-                    {
-                        while (dr.Read())
-                        {
-                            Author author = LoadAuthorForReader(dr);
-
-                            result.Add(author);
-                        }
-                    }
-
-                }
-
-            }
-            return result;
-        }
-
-        private static Author LoadAuthorForReader(SqlDataReader dr)
-        {
-            int authorID = (int)dr["ID"];
-            string authorName = (string)dr["Name"];
-            DateTime? dob = dr.IsDBNull(2) ? (DateTime?)null : (DateTime)dr["DateOfBirth"];
-            DateTime? dod = dr.IsDBNull(3) ? (DateTime?)null : (DateTime)dr["DateOfDeath"];
-
-            Author author = new Author
-            {
-                Id = authorID,
-                Name = authorName,
-                BirthDay = dob,
-                DeathDay = dod
-            };
-            return author;
         }
     }
 }
