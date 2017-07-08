f7edfe5de1a3d661676a4ca9368d6c0c69491cfc
Wekoslav Stefanovski <swekster@gmail.com>
2017-07-08 12:02:42 +0200

    added dapper

---
+++ DapperDemo/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public DateTime? DateOfDeath { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{ID}: {Name} ({DateOfBirth} - {DateOfDeath})";
+        }
+    }
+}
---
+++ DapperDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["BooksDb"].ConnectionString;
+            var connection = new SqlConnection(connectionString);
+
+            var result = connection.Query<Author>("select * from Authors");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
